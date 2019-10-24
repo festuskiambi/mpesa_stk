bbd607022c6efd9c8d8864fd2312c4a19d8548f8
Samuel Ralak <thesamuelralak@gmail.com>
2019-10-24 15:07:42 +0300

    Created new configuration class

--- lib/mpesa_stk.rb
+++ lib/mpesa_stk.rb
@@ -1,4 +1,22 @@
 require "mpesa_stk/version"
-require 'mpesa_stk/push_payment'
-require 'dotenv/load'
-require 'httparty'
+require "mpesa_stk/configuration"
+require "mpesa_stk/push_payment"
+require "httparty"
+
+module MpesaStk
+  class << self
+    attr_writer :configuration
+  end
+
+  def self.configuration
+    @configuration ||= Configuration.new
+  end
+
+  def self.reset
+    @configuration = Configuration.new
+  end
+
+  def self.configure
+    yield(configuration)
+  end
+end
---
+++ lib/mpesa_stk/configuration.rb
@@ -0,0 +1,19 @@
+module MpesaStk
+  class Configuration
+    attr_accessor :daraja_base_url,
+                  :consumer_key,
+                  :consumer_secret,
+                  :business_short_code,
+                  :business_pass_key
+
+    def initialize
+      # Values are set because default values are not provided and must be set by the user
+
+      @daraja_base_url = nil
+      @consumer_key = nil
+      @consumer_secret = nil
+      @business_short_code = nil
+      @business_pass_key = nil
+    end
+  end
+end
